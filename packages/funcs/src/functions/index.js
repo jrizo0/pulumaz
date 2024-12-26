445d3d642edc4acf9c45d092260978a8ade268de
jrizo0 <jr.rizo.o.jr@gmail.com>
2024-12-26 14:21:17 -0500

    WIP try fix funcs not loading

---
+++ packages/funcs/src/functions/index.js
@@ -0,0 +1,11 @@
+//
+const { app } = require("@azure/functions");
+
+app.http("helloWorld1", {
+  route: "hello/world",
+  handler: async (request, context) => {
+    return {
+      body: "Hello world!",
+    };
+  },
+});
