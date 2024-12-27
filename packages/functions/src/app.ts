4357c1355ae0d5c8a90a8b52530ba46698d92623
jrizo0 <jr.rizo.o.jr@gmail.com>
2024-12-27 11:09:57 -0500

    feat: core in funcs

--- packages/functions/src/app.ts
+++ packages/functions/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { Example } from '@pulumaz/core/src/example'
 
 export const api = new Hono()
   .use(logger())
@@ -8,7 +9,7 @@ export const api = new Hono()
     return c.text("Hello Azure Functions!");
   })
   .get("/helloworld", (c) => c.text("Hello world"))
-  // .get("/core", (c) => c.text(Example.hello()))
+  .get("/core", (c) => c.text("From core package:" + Example.hello()))
   .get("/error", (c) => {
     console.log("We're going to throw an error");
     throw new Error("mock error");
