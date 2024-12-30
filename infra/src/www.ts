95ec238418c599060b346788b3f59010625ef736
jrizo0 <jr.rizo.o.jr@gmail.com>
2024-12-30 09:09:35 -0500

    WIP try www deploy

---
+++ infra/src/www.ts
@@ -0,0 +1,28 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure-native";
+import * as random from "@pulumi/random";
+
+// Create an Azure Resource Group
+const resourceGroup = new azure.resources.ResourceGroup("resourceGroup", {
+  location: "canadacentral",
+});
+
+// Create an Azure Static Web App
+const staticWebApp = new azure.web.StaticSite("wwwappnextjs", {
+  resourceGroupName: resourceGroup.name,
+  location: resourceGroup.location,
+  sku: {
+    name: "Free",
+    tier: "Free",
+  },
+  repositoryUrl: "https://github.com/jrizo0/pulumaz",
+  branch: "main",
+  buildProperties: {
+    appLocation: "packages/www/",
+    outputLocation: "packages/www/.next",
+    appBuildCommand: "npm run build:www"
+  },
+});
+
+// Export the URL of the static web app
+export const staticWebAppUrl = staticWebApp.defaultHostname;
